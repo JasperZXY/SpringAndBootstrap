bf2432bffcd29352a3486117a562a83118242e84
JasperZXY <jasperzxy@gmail.com>
2015-04-06 15:22:38 +0800

    init my spring listener to get all beans

--- src/main/java/com/jasper/sab/web/controller/TestController.java
+++ src/main/java/com/jasper/sab/web/controller/TestController.java
@@ -1,11 +1,22 @@
 package com.jasper.sab.web.controller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("test")
 public class TestController {
+	
+	@Value("${logs_level}")
+	private String str;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("=============" + str + "==========");
+	}
 	
 	@RequestMapping("index")
 	public String index() {
